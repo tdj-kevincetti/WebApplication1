1b3ba40a022bf7af6e68f2943a48e233d4c5bd09
tdj-kevincetti <timezainw2000@gmail.com>
2022-02-27 16:57:29 +0700

    Add project files.

---
+++ WebApplication1/Controllers/LoginCtrl.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginCtrl : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProcessLogin(Staff staffd)
+        {
+            if (staffd.Username == "FrancisPD" && staffd.Password=="Kuy"){
+                return View("managetableUI", staffd);
+            }
+            else
+            {
+                return View("LoginFailure", staffd);
+            }
+        }
+    }
+}
